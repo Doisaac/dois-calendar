44291fa9e25cb2a107c6489da1be81524d946e20
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-08 15:33:28 -0600

    feat: add calendar localizer helper for date-fns integration

---
+++ src/helpers/calendarLocalizer.ts
@@ -0,0 +1,18 @@
+import { dateFnsLocalizer } from 'react-big-calendar'
+import { format, parse, startOfWeek, getDay } from 'date-fns'
+import { enUS } from 'date-fns/locale/en-US'
+import { es } from 'date-fns/locale/es'
+
+
+const locales = {
+  'en-US': enUS,
+  'es': es,
+}
+
+export const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
