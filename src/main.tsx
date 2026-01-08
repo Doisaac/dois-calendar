2017516184d60a17d0624d97fe7b5f941850987c
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-08 15:32:41 -0600

    refactor: remove StrictMode wrapper from rendering of DoisCalendar to avoid button problems

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { DoisCalendar } from './DoisCalendar'
 import './style.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <DoisCalendar />
-  </StrictMode>
-)
+createRoot(document.getElementById('root')!).render(<DoisCalendar />)
