8d9da9a98cab01d170aa724a40b4435f29571104
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-14 11:20:41 -0600

    feat: add middleware configuration to store for serializable check

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,10 @@ export const store = configureStore({
     ui: UiReducer,
     calendar: CalendarReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
