1b28d1117298733a4927409eb85588254375a404
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2026-01-24 14:34:34 -0600

    feat: add loader component

---
+++ src/router/ui/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+      <span className="loader"></span>
+    </div>
+  )
+}
